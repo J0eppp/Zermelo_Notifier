765e66909a66233be08faacfdbcdc745ac530320
J0eppp <joep.van.dijk04@gmail.com>
2021-12-04 12:29:55 +0100

    Add start with Java bot for fun

---
+++ ZermeloDiscordNotifier/src/main/java/dev/j0eppp/zermelodiscordnotifier/ZermeloDiscordNotifier.java
@@ -0,0 +1,48 @@
+package dev.j0eppp.zermelodiscordnotifier;
+
+import discord4j.core.DiscordClient;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+interface Command {
+    void execute(MessageCreateEvent event);
+}
+
+public class ZermeloDiscordNotifier {
+
+    private static final Map<String, Command> commands = new HashMap<>();
+
+    static {
+        commands.put("ping", event -> event.getMessage()
+                .getChannel().block()
+                .createMessage("Pong!").block());
+    }
+
+    public static void main(String[] args) {
+        GatewayDiscordClient client = DiscordClient.create(args[0]).login().block();
+        
+        client.getEventDispatcher().on(MessageCreateEvent.class)
+                // subscribe is like block, in that it will *request* for action
+                // to be done, but instead of blocking the thread, waiting for it
+                // to finish, it will just execute the results asynchronously.
+                .subscribe(event -> {
+                    // 3.1 Message.getContent() is a String
+                    final String content = event.getMessage().getContent();
+
+                    for (final Map.Entry<String, Command> entry : commands.entrySet()) {
+                        // We will be using ! as our "prefix" to any command in the system.
+                        if (content.startsWith('!' + entry.getKey())) {
+                            entry.getValue().execute(event);
+                            break;
+                        }
+                    }
+                });
+
+
+        client.onDisconnect().block();
+    }
+
+}
